0465cfaa45b0698f3b80dd9b7db6c4223f0c7851
unknown <gabriel.santos1518@etec.sp.gov.br>
2022-09-12 18:02:56 -0300

    liscli1209

--- JAVASCRIPT/controle.js
+++ JAVASCRIPT/controle.js
@@ -15,4 +15,20 @@ function fechaconf() {
     document.getElementById("linksconf").style.transitionDelay = "0s";
 }
 
+
+function abreconf2() {
+    document.getElementById("cadcli_lis").style.width = "750px";
+    document.getElementById("cadcli_lis").style.borderLeft = "1px solid black";
+    document.getElementById("cadcli_lis").style.borderBottom = "1px solid black";
+    document.getElementById("linksconf").style.opacity = 1;
+}
+
+function fechaconf2() {
+    document.getElementById("cadcli_lis").style.width = "0px";
+    document.getElementById("cadcli_lis").style.borderLeft = "0px";
+    document.getElementById("cadcli_lis").style.borderBottom = "0px solid black";
+    document.getElementById("linksconf").style.opacity = 0;
+    document.getElementById("linksconf").style.transitionDelay = "0s";
+}
+
   
