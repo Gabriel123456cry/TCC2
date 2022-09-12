e22ddbc100d4fa61592f2e8158ac329fe1f32f9c
unknown <gabriel.santos1518@etec.sp.gov.br>
2022-09-11 21:03:10 -0300

    listagem_cli_11-09

--- JAVASCRIPT/controle.js
+++ JAVASCRIPT/controle.js
@@ -1,23 +1,18 @@
-let tamanho = 0
+// funções para abrir e fechar a div configurações
 
 function abreconf() {
-    document.getElementById("config-aba").style.width = "225px";
-    
-    tamanho = document.getElementById("config-aba").style.width
-    
-    if(mouseover = "225px"){
-        setInterval(() => {
-        document.getElementById("config-aba");
-        addEventListener('mouseleave', fechaconf);
-
-    }, 1000)
-}
-
+    document.getElementById("config-aba").style.width = "250px";
+    document.getElementById("config-aba").style.borderLeft = "1px solid black";
+    document.getElementById("config-aba").style.borderBottom = "1px solid black";
+    document.getElementById("linksconf").style.opacity = 1;
 }
 
 function fechaconf() {
     document.getElementById("config-aba").style.width = "0px";
-    
-
+    document.getElementById("config-aba").style.borderLeft = "0px";
+    document.getElementById("config-aba").style.borderBottom = "0px solid black";
+    document.getElementById("linksconf").style.opacity = 0;
+    document.getElementById("linksconf").style.transitionDelay = "0s";
 }
 
+  
