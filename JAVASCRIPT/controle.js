0a4330b252c37e5208077b11d904561daba72060
unknown <gabriel.santos1518@etec.sp.gov.br>
2022-09-13 23:20:58 -0300

    cadope

--- JAVASCRIPT/controle.js
+++ JAVASCRIPT/controle.js
@@ -1,14 +1,14 @@
 // funções para abrir e fechar a div configurações
 
 function abreconf() {
-    document.getElementById("config-aba").style.width = "250px";
+    document.getElementById("config-aba").style.height = "400px";
     document.getElementById("config-aba").style.borderLeft = "1px solid black";
     document.getElementById("config-aba").style.borderBottom = "1px solid black";
     document.getElementById("linksconf").style.opacity = 1;
 }
 
 function fechaconf() {
-    document.getElementById("config-aba").style.width = "0px";
+    document.getElementById("config-aba").style.height = "0px";
     document.getElementById("config-aba").style.borderLeft = "0px";
     document.getElementById("config-aba").style.borderBottom = "0px solid black";
     document.getElementById("linksconf").style.opacity = 0;
